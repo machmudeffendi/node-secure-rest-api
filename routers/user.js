c44d1976ab9c9a484a00e8fffc9c107fd49c6c80
Machmud Effendi <machmudeffendi01@gmail.com>
2020-03-19 09:53:09 +0700

    Update: Email sender

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,11 @@ const express = require('express');
 const User = require('../models/User');
 const auth = require('../middleware/auth');
 
+var MailConfig = require('../config/email');
+var hbs = require('nodemailer-express-handlebars');
+var gmailTransport = MailConfig.GmailTransport;
+var smtpTransport = MailConfig.SMTPTransport;
+
 const router = express.Router();
 
 router.post('/users', async (req, res) => {
@@ -61,5 +66,59 @@ router.post('/users/me/logoutall', auth, async (req, res) => {
         res.status(500).send(error);
     }
 })
+
+router.get('/users/email/template', (req, res, next) => {
+  MailConfig.ViewOption(gmailTransport,hbs);
+  let HelperOptions = {
+    from: 'machmudeffendi01@gmail.com',
+    to: 'machmudeffendi0@gmail.com',
+    subject: 'Hellow world!',
+    template: 'test',
+    context: {
+      name:"Machmud effendi",
+      email: "machmudeffendi01@gmail.com",
+      address: "Yogyakarta, Indonesia"
+    }
+  };
+  gmailTransport.sendMail(HelperOptions, (error,info) => {
+    if(error) {
+      console.log(error);
+      res.json(error);
+    }
+    console.log("email is send");
+    console.log(info);
+    res.json(info)
+  });
+});
+
+router.get('/users/email/smtp/template', (req, res, next) => {
+  MailConfig.ViewOption(smtpTransport,hbs);
+  let HelperOptions = {
+    from: '"Tariqul islam" <tariqul@falconfitbd.com>',
+    to: 'tariqul.islam.rony@gmail.com',
+    subject: 'Hellow world!',
+    template: 'test',
+    context: {
+      name:"tariqul_islam",
+      email: "tariqul.islam.rony@gmail.com",
+      address: "52, Kadamtola Shubag dhaka"
+    }
+  };
+  smtpTransport.verify((error, success) => {
+      if(error) {
+        res.json({output: 'error', message: error})
+        res.end();
+      } else {
+        smtpTransport.sendMail(HelperOptions, (error,info) => {
+          if(error) {
+            res.json({output: 'error', message: error})
+          }
+          res.json({output: 'success', message: info});
+          res.end();
+        });
+      }
+  })
+  
+});
 
 module.exports = router;
